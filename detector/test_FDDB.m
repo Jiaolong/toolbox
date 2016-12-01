b762217f544dfe8832e730cb8228183d4f712f5d
jiaolong <jl.xu@ulsee.com>
2016-12-01 18:38:45 +0800

    added test code

---
+++ detector/test_FDDB.m
@@ -0,0 +1,51 @@
+% Test on FDDB dataset
+addpath(genpath('..'));
+show = false;
+data_dir = fullfile('..', 'data', 'Data_FDDB');
+
+model_file = fullfile('models', 'FaceDetector.mat');
+% Load detector
+load(model_file);
+% modify detector (see acfModify)
+pNms = opts.pNms;
+pNms.overlap = 0.5;
+pModify=struct('cascThr',-1,'cascCal',-0.008, 'pNms', pNms);
+detector=acfModify(detector,pModify);
+
+% Run detector
+num_folds = 10;
+for i=1:num_folds
+    fprintf('Detecting on fold %d/%d\n', i, num_folds);
+    flist_file = fullfile(data_dir, 'FDDB-folds',...
+        sprintf('FDDB-fold-%02d.txt', i));
+
+    % read image names
+    fid = fopen(flist_file, 'r');
+    C = textscan(fid, '%s');
+    fclose(fid);
+    img_names = C{1};
+    num_images = length(img_names);
+    boxes_list = cell(num_images,1);
+    % detect on images
+    parfor j=1:num_images
+        img_file = sprintf('%s/%s.jpg', data_dir, img_names{j});
+        I = imread(img_file);
+        bbs = acfDetect(I, detector);
+        if show
+            figure(1); im(I); bbApply('draw',bbs); pause(.1);
+        end
+        % Write detections into file
+        boxes_list{j} = bbs;
+    end
+    % write detections into file
+    save_file = fullfile(data_dir, 'detections', sprintf('fold-%02d-out.txt', i));
+    fid = fopen(save_file,'w');
+    for j=1:num_images
+        fprintf(fid, '%s\n', img_names{j});
+        boxes = boxes_list{j};
+        fprintf(fid, '%d\n', size(boxes, 1));
+        for b=1:size(boxes,1)
+            fprintf(fid, '%f %f %f %f %f\n', boxes(b, :));
+        end
+    end
+end
---
+++ detector/test_WIDER.m
@@ -0,0 +1,40 @@
+% Test a ACF face detector on WIDER validation set
+
+show = false;
+data_dir = fullfile('..', 'data', 'Data_WIDER');
+images_root = fullfile(data_dir, 'WIDER_Val', 'images');
+
+model_file = fullfile('models', 'FaceDetector.mat');
+
+% Read validation set
+load(fullfile(data_dir, 'v1', 'wider_face_val.mat'));
+event_num = 61;
+pred_list = cell(event_num, 1);
+
+% Load detector
+load(model_file);
+% modify detector (see acfModify)
+pModify=struct('cascThr',-1,'cascCal',-0.008);
+detector=acfModify(detector,pModify);
+
+% Run detector
+for i=1:event_num
+    fprintf('Detection: current event %d\n', i);
+    img_list = file_list{i};
+    img_num = size(img_list, 1);
+    bbox_list = cell(img_num, 1);
+    for j=1:img_num
+        img_file = sprintf('%s/%s/%s.jpg', images_root, event_list{i}, img_list{j});
+        I = imread(img_file);
+        tic, bbs = acfDetect(I, detector); toc
+        if show
+            figure(1); im(I); bbApply('draw',bbs); pause(.1);
+        end
+        % Write detections into file
+        bbox_list{j} = bbs;
+    end
+    pred_list{i} = bbox_list;
+end
+
+% save detections
+save(fullfile(data_dir, 'eval_tools', 'detections', 'acf_dlib_train.mat'), 'pred_list');
